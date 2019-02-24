e53384d14460af6b740c0d89c2e8b1ff7df9b556
Shergill, Daljit <Daljit.Shergill@experian.com>
2019-02-24 16:48:12 +0000

    uploaded site via ftp.. also created thank you page

--- src/views/templates/mail.php
+++ src/views/templates/mail.php
@@ -1,10 +1,12 @@
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
+$phone = $_POST['phone'];
+$formcontent="From: $name \n Message: $message \n Phone: $phone";
 $recipient = "d4lj1t@hotmail.com";
-$subject = "Contact Form";
+$subject = "Thurney Properties Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+header('Location: thank-you.html');
+exit;
 ?>
